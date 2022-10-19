3c9be24121534196c192d84b03f3775d91e4c5d5
gytis <gytis.lazauskas@icloud.com>
2022-10-19 09:30:34 +0300

    initial commit

---
+++ apps/host/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes, {})],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
